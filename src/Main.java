0b443fd1de2b7362fcd01ed1359ef33bdc3e8799
Rohith <rohithmakam05@gmail.com>
2025-07-31 17:17:50 +0530

    removed unnecessary things from main class

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-//        Item coke = new Item("Drink","Coke",1.50);
-//        coke.printItem();
-//        coke.setSize("LARGE");
-//        coke.printItem();
-//
-//        Item avocado = new Item("TOPPINGS","Avocado",1.50);
-//        avocado.printItem();
 
-
-//        Burger burger = new Burger("regular", 4.00);
-//        burger.addToppings("BACON", "CHEESE", "MAYO");
-//        burger.printItem();
 
 //        MealOrder regularMeal = new MealOrder();
 //        regularMeal.addBurgerTopping("BACON", "CHEESE", "MAYO");
 //        regularMeal.setDrinkSize("LARGE");
 //        regularMeal.printItemizedList();
 
-        MealOrder secondOrder = new MealOrder("deluxe", "Pepsi", "chilli");
+        MealOrder secondOrder = new MealOrder("dulex", "Pepsi", "chilli");
         secondOrder.setDrinkSize("SMALL");
         secondOrder.addBurgerTopping("LETTUCE", "CHEESE", "MAYO", "AVOCADO","BACON");
         secondOrder.printItemizedList();
